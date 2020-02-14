db6a152fdbe205e86540efbf137fbeaf99b99a11
Lucas Varela <lucasmouravarela@gmail.com>
2020-02-14 17:27:16 -0300

    Starting project

--- react-app/src/Principal.js
+++ react-app/src/Principal.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import {Button, TextField, Grid, Divider} from '@material-ui/core';
+import Navbar from './components/Navbar';
+import MeuCard from './components/MeuCard';
+import './Principal.css';
 
 export default 	function Principal() {
-	return <div>
-		<div>Navbar</div>
-		<div>Conteudo</div>
-	</div>;
+	return <div style={{paddingLeft: '5%', paddingRight: '5%'}}>
+				<Navbar/>
+
+				<Divider style={{marginTop: 20, marginBottom: 20}}/>
+
+				<Grid container spacing="6">
+					<Grid item md="4"><MeuCard conteudoCor="#fff" titulo="Entrada" cor="#2196f3" valor="2500"/></Grid>
+					<Grid item md="4"><MeuCard conteudoCor="#fff" titulo="Saída" cor="#3f51b5" valor="2800"/></Grid>
+					<Grid item md="4"><MeuCard conteudoCor="#fff" titulo="Balanço" cor="#673ab7" valor={2500 - 2800}/></Grid>
+				</Grid>
+			</div>;
 }
